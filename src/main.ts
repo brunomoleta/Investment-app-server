dbd119848436ef9204eaf6c9af965e4a05fb382d
Bruno Moleta <brunomoleta@pm.me>
2024-01-30 19:17:37 -0300

    Add CORS configuration and enable it in the application
    A CORS setup has been added to the bootstrap function in the main.ts file.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,24 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  // Your CORS configuration code:
+  const ngrokPattern = /https:\/\/[a-zA-Z0-9-]+\.ngrok-free\.app(.*)/;
+  const regexPattern: RegExp = /.durvalmusicshop.*\.vercel\.app.*/;
+  const localUrl: string = 'http://localhost:3000';
+  const acceptedValuesArray = [regexPattern, localUrl, ngrokPattern];
+
+  const corsOptions = {
+    origin: acceptedValuesArray,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  };
+
+  // Enable CORS with the given options:
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
+
 bootstrap();
