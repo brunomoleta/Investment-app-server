5861ee8f5b8ebebb87eefda3868c82df57eb1854
Bruno Moleta <brunomoleta@pm.me>
2024-02-01 19:48:09 -0300

    Remove App controller and update App module imports
    Removed the App controller and its related service. Also updated the App module to import the newly created modules 'Admins', 'Investors', 'Advisors', 'Session', 'InvestmentType'.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AdminsModule } from './modules/admin/admins.module';
+import { InvestorsModule } from './modules/investors/investors.module';
+import { AdvisorsModule } from './modules/advisors/advisors.module';
+import { SessionModule } from './modules/session/session.module';
+import { InvestmentTypeModule } from './modules/investment_types/investmentType.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AdminsModule,
+    InvestorsModule,
+    AdvisorsModule,
+    SessionModule,
+    InvestmentTypeModule
+  ],
 })
 export class AppModule {}
