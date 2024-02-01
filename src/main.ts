b072fb0aecb8a11b5cb22a2b1c22f10de314b91b
Bruno Moleta <brunomoleta@pm.me>
2024-02-01 19:49:47 -0300

    Implement validation pipes and Swagger in main.ts
    Added ValidationPipe for global application use and generated a Swagger API documentation setup in main.ts.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true }),
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { groups: ['transform'] },
+    }),
+  );
 
-  // Your CORS configuration code:
   const ngrokPattern = /https:\/\/[a-zA-Z0-9-]+\.ngrok-free\.app(.*)/;
   const regexPattern: RegExp = /.durvalmusicshop.*\.vercel\.app.*/;
   const localUrl: string = 'http://localhost:3000';
@@ -17,10 +25,26 @@ async function bootstrap() {
     credentials: true,
     optionsSuccessStatus: 204,
   };
-
-  // Enable CORS with the given options:
   app.enableCors(corsOptions);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle("Mo'money less problems app Back-end")
+    .setDescription(
+      'Back-end nest.js app that connects investors with investment advisors,
+    )
+    .setVersion('1.0.0')
+    .addTag('login')
+    .addTag('advisors')
+    .addTag('investors')
+    .addTag('intestment-types')
+    .addTag('products')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
+  const port = Number(process.env.PORT) || 3001;
+
+  await app.listen(port);
 }
 
 bootstrap();
