1918a0e6452c646f8bc36173ca62f9138f4db085
Bruno Moleta <brunomoleta@pm.me>
2024-02-02 18:18:19 -0300

    Implement distinct login methods for different user types.
    The code changes involve implementing distinct login methods for admin, advisor, and investor user types in the session controllers.

--- src/modules/session/session.controller.ts
+++ src/modules/session/session.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Body, Param, Delete, HttpCode, Patch } from '@ne
 import { ApiTags } from '@nestjs/swagger';
 import { SessionService } from './session.service';
 import { StartSessionDto } from './dto/start-session.dto';
+import { AdvisorsService } from '../advisors/advisors.service';
+import { InvestorsService } from '../investors/investors.service';
 
 @ApiTags('login')
 @Controller('session')
@@ -9,8 +11,19 @@ export class SessionController {
   constructor(private readonly sessionService: SessionService) {
   }
 
-  @Post()
-  async login(@Body() admin: StartSessionDto){
-    return this.sessionService.login(admin)
+  @Post('/admin')
+  async loginAdmin(@Body() admin: StartSessionDto) {
+    return this.sessionService.login({ ...admin, userType: 'admin' });
   }
+
+  @Post('/advisor')
+  async loginAdvisor(@Body() advisor: StartSessionDto) {
+    return this.sessionService.login({ ...advisor, userType: 'advisor' });
+  }
+
+  @Post('/investor')
+  async loginInvestor(@Body() investor: StartSessionDto) {
+    return this.sessionService.login({ ...investor, userType: 'investor' });
+  }
+
 }
--- src/modules/session/session.service.ts
+++ src/modules/session/session.service.ts
@@ -5,6 +5,8 @@ import { AdminsService } from '../admin/admins.service';
 import { JwtService } from '@nestjs/jwt';
 import { StartSessionDto } from './dto/start-session.dto';
 import { compare } from 'bcryptjs';
+import { InvestorsService } from '../investors/investors.service';
+import { AdvisorsService } from '../advisors/advisors.service';
 
 
 @Injectable()
@@ -12,28 +14,45 @@ export class SessionService {
   constructor(
     private readonly prisma: PrismaService,
     private adminService: AdminsService,
+    private investorsService: InvestorsService,
+    private advisorsService: AdvisorsService,
     private jwtService: JwtService,
   ) {
   }
 
-  async login({ email, password }: StartSessionDto) {
-    if (!email || !password) {
-      throw new BadRequestException(
-        'Email and password must be provided');
+  async login({ email, password, userType }: StartSessionDto & { userType: 'admin' | 'investor' | 'advisor' }) {
+    let user;
+
+    switch (userType) {
+      case 'admin':
+        user = await this.adminService.findByEmail(email);
+        break;
+      case 'investor':
+        // you will need to create and inject InvestorsService
+        user = await this.investorsService.findByEmail(email);
+        break;
+      case 'advisor':
+        user = await this.advisorsService.findByEmail(email);
+        break;
+      default:
+        throw new BadRequestException('Invalid user type');
     }
 
-    const admin = await this.adminService.findByEmail(email);
-
-    if (!admin) {
+    if (!user) {
       throw new UnauthorizedException('Invalid email or password');
     }
-    const passwordMatch = await compare(password, admin.password);
+
+    const passwordMatch = await compare(password, user.password);
     if (!passwordMatch) {
       throw new UnauthorizedException('Invalid email or password');
     }
 
-    const token = this.jwtService.sign(
-      { email, id: admin.id });
-    return { token };
+    const token = this.jwtService.sign({
+      id: user.id,
+      access_type: user.access_type,
+    });
+
+
+    return { token: token, access_type: user.access_type };
   }
 }
