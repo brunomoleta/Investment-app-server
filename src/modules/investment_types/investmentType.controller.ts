0a9a97b62c0139080744020e7cfb1e30b403416f
Bruno Moleta <brunomoleta@pm.me>
2024-02-03 12:22:28 -0300

    Enhance investment type service and controller functions
    Updated the "InvestmentType" service and controller to add role-based access control and new filtering options. The updated service now includes functions for filtering by risk and for retrieving all investment types, exclusive to admin users.

--- src/modules/investment_types/investmentType.controller.ts
+++ src/modules/investment_types/investmentType.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Post, Body, Param, Delete, HttpCode, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../session/jwt.guard';
 import { InvestmentTypeService } from './investmentType.service';
-import { CreateInvestmentTypeDto } from './dto/create-investmentType.dto';
+import { CreateInvestmentTypeDto, Risk } from './dto/create-investmentType.dto';
+import { Roles, UserRole } from '../../decorators/roles.decorator';
+import { RolesGuard } from '../../decorators/roles.guard';
+import { UpdateInvestmentTypeDto } from './dto/update-investmentType.dto';
 
 
 @Controller('type')
@@ -9,9 +12,18 @@ export class InvestmentTypeController {
   constructor(private readonly investmentTypeService: InvestmentTypeService) {
   }
 
+  @Roles(UserRole.Advisor)
+  @UseGuards(JwtGuard, RolesGuard)
   @Post()
   create(@Body() createInvestmentTypeDto: CreateInvestmentTypeDto) {
     return this.investmentTypeService.create(createInvestmentTypeDto);
+  }
+
+  @Get('all')
+  @Roles(UserRole.Admin)
+  @UseGuards(JwtGuard, RolesGuard)
+  findAllAdminOnly() {
+    return this.investmentTypeService.findAllAdminOnly();
   }
 
   @Get()
@@ -19,30 +31,34 @@ export class InvestmentTypeController {
     return this.investmentTypeService.findAll();
   }
 
-  @UseGuards(JwtGuard)
+  @Get(':risk')
+  filteredByRisk(@Param('risk') risk: Risk) {
+    return this.investmentTypeService.filteredByRisk(risk);
+  }
+
   @Get(':name')
   findByName(@Param('name') type_name: string) {
     return this.investmentTypeService.findByName(type_name);
   }
 
-  @UseGuards(JwtGuard)
   @Get(':id')
   findById(@Param('id') id: string) {
     return this.investmentTypeService.findById(id);
   }
 
-  // @UseGuards(JwtGuard)
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateAdvisorDto: UpdateAdvisorDto) {
-  //   return this.investmentTypeService.update(id, updateAdvisorDto);
-  // }
+  @Patch(':id')
+  @Roles(UserRole.Advisor)
+  @UseGuards(JwtGuard, RolesGuard)
+  update(@Param('id') id: string, @Body() updateInvestmentTypeDto: UpdateInvestmentTypeDto) {
+    return this.investmentTypeService.update(id, updateInvestmentTypeDto);
+  }
 
-  @UseGuards(JwtGuard)
   @HttpCode(204)
+  @Roles(UserRole.Advisor)
+  @UseGuards(JwtGuard, RolesGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.investmentTypeService.remove(id);
   }
-
 
 }
--- src/modules/investment_types/investmentType.service.ts
+++ src/modules/investment_types/investmentType.service.ts
@@ -3,9 +3,10 @@ import { ConflictException, Injectable, NotFoundException } from '@nestjs/common
 
 import { PrismaService } from '../../database/prisma.service';
 import { plainToInstance } from 'class-transformer';
-import { CreateInvestmentTypeDto } from './dto/create-investmentType.dto';
+import { CreateInvestmentTypeDto, Risk } from './dto/create-investmentType.dto';
 import { InvestmentType } from './entities/investmentType.entity';
 import { UpdateInvestmentTypeDto } from './dto/update-investmentType.dto';
+import { Advisor } from '../advisors/entities/advisor.entity';
 
 @Injectable()
 export class InvestmentTypeService {
@@ -33,9 +34,58 @@ export class InvestmentTypeService {
     return plainToInstance(InvestmentType, newInvestmentType);
   }
 
-  findAll() {
-    const investment_types = this.prisma.investmentType.findMany();
+  async findAll() {
+    const investment_types = await this.prisma.investmentType.findMany();
     return plainToInstance(InvestmentType, investment_types);
+  }
+
+  async findAllAdminOnly() {
+    const advisors = await this.prisma.investmentType.findMany(
+      {
+        select: {
+          id: true,
+          type_name: true,
+
+          advisors: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              image: true,
+              phone_number: true,
+              experience: true,
+              created_at: true,
+              investors: {
+                select: {
+                  name: true,
+                  amount: true,
+                  created_at: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    );
+    return plainToInstance(Advisor, advisors);
+  }
+
+  async filteredByRisk(risk: Risk) {
+    const filteredInvestmentTypes = await this.prisma.investmentType.findMany({
+      where: { risk },
+      select: {
+        type_name: true,
+        advisors: {
+          select: {
+            name: true,
+            image: true,
+            created_at: true,
+            experience: true,
+          },
+        },
+      },
+    });
+    return plainToInstance(InvestmentType, filteredInvestmentTypes)
   }
 
   async findByName(name: string) {
