5c7d48de256dd70a92e6258d5dd66cadeb023292
Bruno Moleta <brunomoleta@pm.me>
2024-02-13 17:39:44 -0300

    Add API responses to controllers and set's a README

--- src/modules/admin/admins.controller.ts
+++ src/modules/admin/admins.controller.ts
@@ -13,23 +13,37 @@ import { AdminsService } from './admins.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { JwtGuard } from '../session/jwt.guard';
 import { decode } from 'jsonwebtoken';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('admin')
 export class AdminsController {
   constructor(private readonly adminsService: AdminsService) {}
 
   @Post()
+  @ApiResponse({
+    status: 200,
+    description: 'Create admin',
+  })
+  @UseGuards(JwtGuard)
   create(@Body() createAdminDto: CreateAdminDto) {
     return this.adminsService.create(createAdminDto);
   }
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'List admins',
+  })
   @UseGuards(JwtGuard)
   findAll() {
     return this.adminsService.findAll();
   }
 
   @Get('id')
+  @ApiResponse({
+    status: 200,
+    description: 'Get specific admin through the token',
+  })
   @UseGuards(JwtGuard)
   findById(@Request() request: any) {
     const token = request.headers.authorization.split(' ')[1];
@@ -41,6 +55,10 @@ export class AdminsController {
   @HttpCode(204)
   @Delete()
   @UseGuards(JwtGuard)
+  @ApiResponse({
+    status: 204,
+    description: 'Remove specific admin through the token',
+  })
   remove(@Request() request: any) {
     const token = request.headers.authorization.split(' ')[1];
     const decoded: any = decode(token);
