a23c2f61f622e03fc2356c9200856fb1f528434c
Bruno Moleta <brunomoleta@pm.me>
2024-02-13 16:21:30 -0300

    Refactor pagination handling and add Investment Types interface
    The 'RetrieveInvestmentTypes' interface has been added to streamline the handling of investment types data.

--- src/interceptor/pagination.interceptor.ts
+++ src/interceptor/pagination.interceptor.ts
@@ -11,12 +11,15 @@ export class PaginationInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const httpArgumentsHost = context.switchToHttp();
     const request = httpArgumentsHost.getRequest();
-    let { perPage } = request.query;
-    const { page } = request.query;
+    let { perPage, page } = request.query;
 
     perPage = Number(perPage);
-    if (perPage > 4 || perPage < 1 || isNaN(perPage)) {
-      perPage = 4;
+    page = Number(page);
+    if (perPage > 8 || perPage < 1 || isNaN(perPage)) {
+      perPage = 8;
+    }
+    if (page < 1 || isNaN(page)) {
+      page = 1;
     }
 
     const skip = page ? (Number(page) - 1) * perPage : 0;
