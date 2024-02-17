00250200b3eab4b1d25e9e79e9167be681e204ca
Bruno Moleta <brunomoleta@pm.me>
2024-02-17 13:10:09 -0300

    Improve Swagger documentation and error handling in session module
    This commit updates Swagger annotations in the session module, mainly in the session.controller, start-session.dto, and session.service files..

--- src/modules/session/dto/start-session.dto.ts
+++ src/modules/session/dto/start-session.dto.ts
@@ -1,10 +1,19 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class StartSessionDto {
+  @ApiProperty({
+    type: String,
+    description: 'Required property.',
+  })
   @IsNotEmpty()
   @IsString()
   email!: string;
 
+  @ApiProperty({
+    type: String,
+    description: 'Required property.',
+  })
   @IsNotEmpty()
   @IsString()
   password!: string;
--- src/modules/session/session.controller.ts
+++ src/modules/session/session.controller.ts
@@ -1,5 +1,10 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiNotFoundResponse,
+  ApiResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { SessionService } from './session.service';
 import { StartSessionDto } from './dto/start-session.dto';
 import { UserRole } from '../../decorators/roles.decorator';
@@ -10,11 +15,31 @@ export class SessionController {
   constructor(private readonly sessionService: SessionService) {}
 
   @Post('/admin')
+  @ApiResponse({
+    status: 200,
+    description: 'Admin is logged in.',
+  })
+  @ApiNotFoundResponse({
+    description: "Admin's invalid email or password",
+  })
+  @ApiUnauthorizedResponse({
+    description: "Admin's invalid email or password",
+  })
   async loginAdmin(@Body() admin: StartSessionDto) {
     return this.sessionService.login({ ...admin, userType: UserRole.Admin });
   }
 
   @Post('/advisor')
+  @ApiResponse({
+    status: 200,
+    description: 'Advisor is logged in.',
+  })
+  @ApiNotFoundResponse({
+    description: "Advisor's invalid email or password",
+  })
+  @ApiUnauthorizedResponse({
+    description: "Advisor's invalid email or password",
+  })
   async loginAdvisor(@Body() advisor: StartSessionDto) {
     return this.sessionService.login({
       ...advisor,
@@ -23,6 +48,16 @@ export class SessionController {
   }
 
   @Post('/investor')
+  @ApiResponse({
+    status: 200,
+    description: 'Investor is logged in.',
+  })
+  @ApiNotFoundResponse({
+    description: "Investor's invalid email or password",
+  })
+  @ApiUnauthorizedResponse({
+    description: "Investor's invalid email or password",
+  })
   async loginInvestor(@Body() investor: StartSessionDto) {
     return this.sessionService.login({
       ...investor,
