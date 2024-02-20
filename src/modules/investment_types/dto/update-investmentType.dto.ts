435d124d08fd16bb2362e46aa62dd03d288e801f
Bruno Moleta <brunomoleta@pm.me>
2024-02-20 14:13:57 -0300

    Refactor Dto classes to extend from related CreateDto classes
    Significantly stripped down the `Update*.dto.ts` multiple files by leveraging `PartialType` from `@nestjs/mapped-types`. Rather than manually specifying class properties and validations again, these classes now extend from their related `Create*.dto.ts` classes which helps maintain DRY code and improves maintainability.

--- src/modules/investment_types/dto/update-investmentType.dto.ts
+++ src/modules/investment_types/dto/update-investmentType.dto.ts
@@ -1,35 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateInvestmentTypeDto, Risk } from './create-investmentType.dto';
-import { ApiPropertyOptional } from '@nestjs/swagger';
-import {
-  IsEnum,
-  IsNotEmpty,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { CreateInvestmentTypeDto } from './create-investmentType.dto';
 
 export class UpdateInvestmentTypeDto extends PartialType(
   CreateInvestmentTypeDto,
-) {
-  @ApiPropertyOptional({
-    type: String,
-    description: 'Optional property',
-  })
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(4)
-  @MaxLength(32)
-  type_name?: string;
-
-  @ApiPropertyOptional({
-    type: String,
-    description:
-      "Optional property. If sent should be either: 'low',\n" +
-      ",'moderate',\n" +
-      ",'high',\n" +
-      "or 'very_high'.",
-  })
-  @IsEnum(Risk)
-  risk?: Risk;
-}
+) {}
